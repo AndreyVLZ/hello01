30e8f18661a89d1fa0b2d5a2d0d53586b759fca5
AndreyVLZ <root@workspace7a6fd5tli0yabz3c.go-cli-76f8bc966f-jrm4k>
2020-01-14 15:08:31 +0000

    1111

--- test1/test13.go
+++ test1/test13.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello, Andrey")
+	bot, err := tgbotapi.NewBotAPI("TOKEN")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+
 }
